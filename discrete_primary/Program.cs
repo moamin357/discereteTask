9b81840fd49a3a1dee2bff0f82c95bc849589388
MohamedEzat03 <121137273+MohamedEzat03@users.noreply.github.com>
2022-12-29 15:59:01 +0200

    second commit
    second commit

---
+++ discrete_primary/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discerete_task_primary_number_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("first number");
+            int first = int.Parse(Console.ReadLine());
+            Console.WriteLine("second number");
+            int last = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = first; i <= last; i++)
+            {
+                for (int x = 1; x <= i; x++)
+                {
+                    if (i % x == 0)
+                        sum++;
+                }
+                if (sum == 2)
+                {
+                    Console.WriteLine(i);
+                    sum = 0;
+                }
+                else
+                {
+                    sum = 0;
+
+
+
+                }
+
+
+            }
+            Console.ReadKey();
+        }
+
+    
+    }
+}
